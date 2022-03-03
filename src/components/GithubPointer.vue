615088e420ecc0214c09904b9a38eabfd79ef706
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-03 18:39:48 +0100

    Create GithubPointer.vue

---
+++ src/components/GithubPointer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="tailwind" class="w-screen hidden lg:flex">
+        <div id="box" class="bg-[#262626]">
+            <p>Check it out on GitHub</p>
+            <img src="../assets/github.png">
+        </div>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap')
+
+    $boxbg: #262626
+    $text: #fff
+    $fontglobal: 'Open Sans', sans-serif
+    * 
+        font-family: $fontglobal
+        color: $text
+        #box
+            position: fixed
+            right: 2vw
+            bottom: 2vw
+            height: 3vw
+            width: 10vw
+            //background: $boxbg
+            border-radius: 1vw
+            padding: 0.25vw
+            img
+                width: 2.5vw
+                height: auto
+                filter: invert(100%) 
+            p 
+                font-size: 0.9vw
+                width: 60%
+                right: 0.35vw
+                position: absolute
+</style>
