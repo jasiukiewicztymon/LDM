bbeb3f23481976955c30c2ec6e76570fd1ffb431
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-03 22:58:03 +0100

    Add files via upload

---
+++ src/src/components/GithubPointer.vue
@@ -0,0 +1,39 @@
+<template>
+    <a href="https://github.com/jasiukiewicztymon/LDM" target="_blank" rel="noopener noreferrer">
+        <div id="tailwind" class="w-screen hidden lg:flex">
+            <div id="box" class="bg-[#262626]">
+                <p>Check it out on GitHub</p>
+                <img src="../assets/github.png">
+            </div>
+        </div>
+    </a>
+</template>
+
+<style lang="sass" scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap')
+
+    $boxbg: #262626
+    $text: #fff
+    $fontglobal: 'Open Sans', sans-serif
+    * 
+        font-family: $fontglobal
+        color: $text
+        #box
+            position: fixed
+            right: 2vw
+            bottom: 2vw
+            height: 3vw
+            width: 10vw
+            cursor: pointer
+            border-radius: 1vw
+            padding: 0.25vw
+            img
+                width: 2.5vw
+                height: auto
+                filter: invert(100%) 
+            p 
+                font-size: 0.9vw
+                width: 60%
+                right: 0.35vw
+                position: absolute
+</style>
---
+++ src/src/components/ModelisationArea.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="main-box" class="absolute left-[50%] -translate-x-[50%] top-[16vw] md:top-[12vw] lg:top-[9vw] w-[90vw] h-[82.5vh] lg:h-[75vh]">
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+    },
+    data() {
+        return {
+           id: 0,
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+
+<style lang="sass" scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap')
+
+    $boxbg: #262626
+    $text: #fff
+    $fontglobal: 'Open Sans', sans-serif
+    * 
+        font-family: $fontglobal
+        #main-box
+            border: 0.25vw solid #000
+            border-radius: 1vw
+</style>
