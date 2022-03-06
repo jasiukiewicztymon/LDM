1e1361d73bbca14606f605b979b80a60954386cc
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-06 21:49:14 +0100

    Update App.vue

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <div id="app">
-    <div class="flex bg-green-100 justify-center items-center h-screen w-screen">
-      <p class="text-white-100">Tailwind</p>
-    </div>
-  </div>
+  <MenuElement/>
+  <ModelisationArea>
+    <slot name="table">
+      <input type="text">
+      <button>Add Value</button>
+    </slot>
+  </ModelisationArea>
+  <GithubPointer/>
 </template>
 
 <script>
+import MenuElement from './components/MenuElement.vue'
+import GithubPointer from './components/GithubPointer.vue'
+import ModelisationArea from './components/ModelisationArea.vue'
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    MenuElement,
+    GithubPointer,
+    ModelisationArea,
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
+  
+<style lang="sass" scoped>
+</style>
