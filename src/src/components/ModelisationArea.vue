0ca82c296658099dfea73e00b38066cb0c1235cc
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-06 21:49:46 +0100

    Update ModelisationArea.vue

--- src/src/components/ModelisationArea.vue
+++ src/src/components/ModelisationArea.vue
@@ -1,5 +1,6 @@
 <template>
-    <div id="main-box" class="absolute left-[50%] -translate-x-[50%] top-[16vw] md:top-[12vw] lg:top-[9vw] w-[90vw] h-[82.5vh] lg:h-[75vh]">
+    <div @click="checkname" id="main-box" class="absolute left-[50%] -translate-x-[50%] top-[16vw] md:top-[12vw] lg:top-[9vw] w-[90vw] h-[82.5vh] lg:h-[75vh]">
+        
     </div>
 </template>
 
@@ -10,10 +11,38 @@ export default {
     data() {
         return {
            id: 0,
+           tables: [],
+           table: {
+               index: 0,
+               title: '',
+               values: [],
+           },
+           value: {
+               name: '',
+               type: '',
+               maxLen: '',
+           },
         }
     },
     methods: {
-        
+        checkname() {
+            if (document.getElementById('plus-info').classList.contains('on')) {
+                this.table.index = this.id;
+                this.table.title = 'test';
+                this.table.value = [1, 2, 3, 4];
+                console.log(this.table)
+                this.id++;
+            }
+            else if (document.getElementById('trash-info').classList.contains('on')) {
+                console.log('2')
+            }
+            else if (document.getElementById('pencil-info').classList.contains('on')) {
+                console.log('3')
+            }
+            else if (document.getElementById('slash-lg-info').classList.contains('on')) {
+                console.log('4')
+            }
+        }
     }
 }
 </script>
