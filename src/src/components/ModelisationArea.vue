a3afe13bd3e7e4a59fa4ffc52f80176abcb19fb8
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-16 16:24:32 +0100

    Update ModelisationArea.vue

--- src/src/components/ModelisationArea.vue
+++ src/src/components/ModelisationArea.vue
@@ -1,7 +1,7 @@
 <template>
     <div @click="checkname" id="main-box" class="absolute left-[50%] overflow-auto -translate-x-[50%] top-[16vw] md:top-[12vw] lg:top-[9vw] w-[90vw] h-[82.5vh] lg:h-[75vh]">
         <div @click.self="containerbox($event)" @mousedown.self="mousedown($event)" @mousemove.self="mousemove($event)" @mouseleave.self="this.drag.isDown = false" @mouseup="this.drag.isDown = false" :class="{active: this.drag.isDown}" class="w-[100000px] h-[100000px] p-[20px]">
-            <div @mousemove="boxmousemove($event, table)" @mouseleave="table.drag.isDown = false" @mouseup="table.drag.isDown = false" @click="box(table)" v-for="table in this.tables" :key="table.id" class="w-[450px] h-[550px] bg-[#41bf82] overflow-hidden overflow-y-auto absolute" :class="[table.top, table.left]" :id="['table-'+table.id]">
+            <div @mousemove="boxmousemove($event, table)" @mouseup="table.drag.isDown = false" @click="box(table)" v-for="table in this.tables" :key="table.id" class="w-[450px] h-[550px] bg-[#41bf82] overflow-hidden overflow-y-auto absolute" :class="[table.top, table.left]" :id="['table-'+table.id]">
                 <div class="w-[100%] h-[100%]" @mousedown="boxmousedown($event, table)">
                     <div class="flex justify-center">
                         <input v-model="table.title" class="p-1 mt-10 w-[70%]">
