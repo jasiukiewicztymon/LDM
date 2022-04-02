cf7cd4d480bc2c7c4ada8d593316ba9b720418b0
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-04-02 20:20:29 +0200

    Update App.vue

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,35 +1,26 @@
+
 <template>
   <MenuElement/>
-  <ModelisationArea>
-    <slot name="table">
-      <input type="text">
-      <button>Add Value</button>
-    </slot>
-  </ModelisationArea>
+  <ModelisationArea/>
   <GithubPointer/>
 </template>
 
 <script>
 import MenuElement from './components/MenuElement.vue'
-import GithubPointer from './components/GithubPointer.vue'
 import ModelisationArea from './components/ModelisationArea.vue'
-
+import GithubPointer from './components/GithubPointer.vue'
 export default {
   name: 'App',
   components: {
     MenuElement,
-    GithubPointer,
     ModelisationArea,
+    GithubPointer
   },
   data() {
     return {
     }
   },
   methods: {
-
   }
 }
 </script>
-  
-<style lang="sass" scoped>
-</style>
